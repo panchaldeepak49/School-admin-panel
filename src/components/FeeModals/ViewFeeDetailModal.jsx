054ecd7d38d891340b5d3a9c9e7bb8d3b13e5965
Deepak <panchaldeepak49@gmail.com>
2024-09-17 15:09:15 +0530

    code push ,17Sept,24

--- src/components/FeeModals/ViewFeeDetailModal.jsx
+++ src/components/FeeModals/ViewFeeDetailModal.jsx
@@ -6,30 +6,30 @@ const ViewFeeDetailModal = ({feeData,setShowFeeDetailModal}) => {
     console.log(feeData);
   return (
     <>
-    <div className='fixed top-10 left-[40%] w-[25%] px-4 py-4 bg-blue-200 rounded-xl border border-green-500'>
-      <img src={feeData.imageUrl} className='w-32' alt='missing'></img>
-      <p className='mt-2 font-medium'>Name : <span className='font-Playwrite'>{feeData.name}</span></p>
-      <div className='grid grid-cols-2 gap-1 mt-4'>
+    <div className='fixed top-2 md:top-10 left-[40%] md:w-[25%] px-4 py-4 bg-blue-200 rounded-xl border border-green-500'>
+      <img src={feeData.imageUrl} className='w-16 md:w-32 rounded-md' alt='missing'></img>
+      <p className='mt-2 text-sm md:text-base font-medium'>Name : <span className='font-Playwrite'>{feeData.name}</span></p>
+      <div className='grid grid-cols-2 gap-1 md:mt-4'>
       
-      <p className='font-medium'>April  :<span>{feeData.april}</span> </p>
-      <p className='font-medium'>May : <span className='font-normal'>{feeData.may}</span></p>
-      <p className='font-medium'>June : <span className='font-normal'>{feeData.june}</span></p>
-      <p className='font-medium'>July : <span className='font-normal'>{feeData.july}</span></p>
-      <p className='font-medium'>August : <span className='font-normal'>{feeData.august}</span></p>
-      <p className='font-medium'>Sept : <span className='font-normal'>{feeData.september}</span></p>
-      <p className='font-medium'>October : <span className='font-normal'>{feeData.october}</span></p>
-      <p className='font-medium'>November : <span className='font-normal'>{feeData.november}</span></p>
-      <p className='font-medium'>December : <span className='font-normal'>{feeData.december}</span></p>
-      <p className='font-medium'>January : <span className='font-normal'>{feeData.january}</span></p>
-      <p className='font-medium'>Feb : <span className='font-normal'>{feeData.feb}</span></p>
-      <p className='font-medium'>March : <span className='font-normal'>{feeData.march}</span></p>
+      <p className='text-sm md:text-base md:font-medium'>Apr  :<span>{feeData.april}</span> </p>
+      <p className='text-sm md:text-base md:font-medium'>May : <span className='font-normal'>{feeData.may}</span></p>
+      <p className='text-sm md:text-base md:font-medium'>June : <span className='font-normal'>{feeData.june}</span></p>
+      <p className='text-sm md:text-base md:font-medium'>July : <span className='font-normal'>{feeData.july}</span></p>
+      <p className='text-sm md:text-base md:font-medium'>Aug : <span className='font-normal'>{feeData.august}</span></p>
+      <p className='text-sm md:text-base md:font-medium'>Sept : <span className='font-normal'>{feeData.september}</span></p>
+      <p className='text-sm md:text-base md:font-medium'>Oct : <span className='font-normal'>{feeData.october}</span></p>
+      <p className='text-sm md:text-base md:font-medium'>Nov : <span className='font-normal'>{feeData.november}</span></p>
+      <p className='text-sm md:text-base md:font-medium'>Dec : <span className='font-normal'>{feeData.december}</span></p>
+      <p className='text-sm md:text-base md:font-medium'>Jan : <span className='font-normal'>{feeData.january}</span></p>
+      <p className='text-sm md:text-base md:font-medium'>Feb : <span className='font-normal'>{feeData.feb}</span></p>
+      <p className='text-sm md:text-base md:font-medium'>March : <span className='font-normal'>{feeData.march}</span></p>
       </div>
-      <p className='mt-4 font-medium'>Amount Accepted : {feeData.totalAmountAccepted}</p>
-      <p className='font-medium'>Amount Due : {feeData.totalAmountDue}</p>
+      <p className='mt-1 md:mt-4 text-sm md:text-base md:font-medium'>Amount Accepted : {feeData.totalAmountAccepted}</p>
+      <p className='text-sm md:text-base md:font-medium'>Amount Due : {feeData.totalAmountDue}</p>
       {/* <p> : {feeData.}</p> */}
      
       
-       <button className='bg-green-500 flex justify-center w-[100%] rounded-md mt-5' onClick={()=>setShowFeeDetailModal(false)}>Close</button>
+       <button className='mt-1 md:mt-5 bg-green-500 flex justify-center w-[100%] rounded-md ' onClick={()=>setShowFeeDetailModal(false)}>Close</button>
     </div>
     </>
   )
