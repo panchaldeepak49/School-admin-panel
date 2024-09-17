c5758fb66b2315975e0d7e0cebd9fea53c5f1406
Deepak <panchaldeepak49@gmail.com>
2024-09-17 16:35:36 +0530

    close button on top

--- src/components/ViewDetailModal.jsx
+++ src/components/ViewDetailModal.jsx
@@ -27,7 +27,10 @@ const ViewDetailModal = ({displayingData,setShowViewModal,classTeacherName}) => 
     <>
     <div className={`fixed top-1 md:top-[4%] left-[40%] flex flex-col gap-1 md:gap-4 py-1 md:py-4 px-4 items-center rounded-md md:w-[25%] ${colorClass} shadow-xl
     border border-blue-600 z-50`}>
+       <div className='w-[100%] flex justify-between'>
         <p className='font-sans  font-semibold md:text-xl text-blue-600 '>Student Details</p>
+        <button className='md:hidden bg-red-500 text-sm md:text-base text-white px-4 py-1 rounded-md animate-pulse' onClick={()=>setShowViewModal(false)}>X</button>
+        </div>
         <div className='w-[100%] flex justify-evenly'>
         <img src={displayingData.imageUrl} className='w-12 h-20 md:h-auto md:w-[30%] rounded-md' alt='oops'></img>
         <FallingLines className='text-sm'  />
@@ -46,7 +49,7 @@ const ViewDetailModal = ({displayingData,setShowViewModal,classTeacherName}) => 
         <p className='text-sm md:text-base font-Rubik'><span className='font-medium md:font-semibold'>Contact : </span> {displayingData.contact}</p>
         </div>
 
-        <div className='flex gap-10'>
+        <div className='hidden md:block '>
             <button className='bg-blue-600 text-sm md:text-base text-white px-4 py-1 rounded-md animate-pulse' onClick={()=>setShowViewModal(false)}>Close</button>
             {/* <button className='bg-orange-400 p-2 rounded-md' onClick={()=>updateUser()}>Update</button> */}
         </div>
